1020ffbce3b7239dc2a4c355eb00ee886cfdf680
1986prasenjit <prasenjitbasu44@gmail.com>
2026-03-24 00:06:44 +0530

    Added few more examples in Coercion, AND OR operator in JS

--- Operators-In-JS/script.js
+++ Operators-In-JS/script.js
@@ -5,7 +5,7 @@
 
 
 //? Arithmetic Operators
-//Addition +
+//Addition + and Addtion is also used to concatenate strings in JavaScript
  2 + 2 //4
 
 //Subtraction -
@@ -30,20 +30,25 @@ let userName = "John"; //Here EqualTo operator is used to assign a value to a va
 
 //? Comparison Operators
 
-//Equal to ==
+//Equal to == 
+//? What does Double Equal to operator do?
+//* Double equal to operator is used to compare two values and return true if they are equal and false if they are not equal. Double equal is also know as Loose Equality Operator because it compares the value not the data type of the value.
 let numThree = 5;
 let numFour = 5;
-console.log(numOne == numTwo); //true
+console.log(numThree == numFour); //true
+
+//Strict equal to ===
+//? What does Triple Equal to operator do?
+//* Triple Equal to often known as Strict Equality Operator in JavaScript because it compares the value as well as the data type of the value.
+let numSeven = 5;
+let numEight = "5";
+console.log(numSeven === numEight); //false
+
 
 //Not equal to !=
 let numFive = 5;
 let numSix = 6;
 console.log(numFive != numSix); //true
-
-//Strict equal to ===
-let numSeven = 5;
-let numEight = "5";
-console.log(numSeven === numEight); //false
 
 //Strict not equal to !==
 let numNine = 5;
@@ -70,3 +75,49 @@ numEleven--; //5
 
 
 //? Logical Operators
+
+//And &&
+let numSixteen = 5;
+let numSeventeen = 6;
+console.log(numSixteen > 0 && numSeventeen > 0); //true
+
+//!Example of && operator
+let numTwentyTwo = 5;
+let numTwentyThree = 0;
+console.log(numTwentyTwo > 0 && numTwentyThree > 0); //false
+
+
+//Or ||
+let numEighteen = 5;
+let numNineteen = 6;
+console.log(numEighteen > 0 || numNineteen > 0); //true
+
+//!Example of || operator
+let numTwentyFour = 5;
+let numTwentyFive = 0;
+console.log(numTwentyFour > 0 || numTwentyFive > 0); //true
+
+
+//Not !
+let numTwenty = 5;
+let numTwentyOne = 6;
+console.log(!(numTwenty > 0 || numTwentyOne > 0)); //false
+
+
+//!Example of ! operator
+let numTwentySix = 5;
+let numTwentySeven = 0;
+console.log(!(numTwentySix > 0 || numTwentySeven > 0)); //true
+
+//!What is COERCION in JavaScript?
+//? COERCION is the automatic conversion of one data type to another data type.
+
+
+//! What is the difference between == and === ?
+//? == is used to compare the value of two variables, but it does not check the data type of the variables and in double equal to COERCION is allowed.
+//? === is used to compare the value and data type of two variables and in triple equal to COERCION is not allowed.
+
+
+//! What is Implecit COERCION and Explicit COERCION?
+//? Implicit COERCION is the automatic conversion of one data type to another data type.Implicit COERCION is done by JavaScript Engine.
+//? Explicit COERCION is the manual conversion of one data type to another data type. Explicit COERCION is done by the user.
